ef464cbfa9b9c6fcfde3311dca592f0c135b1d4f
notrodi <nnotrodi1@gmail.com>
2025-01-28 23:49:40 +0300

    added header and button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
 
 function App() {
+  const user = {
+    name: 'notrodi',
+    age: 24
+  };
+
+  const header2 = <h2>HEADER NUMBER TWO</h2>;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {header2}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {user.name} ({user.age}), edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <Button />
+
         <a
           className="App-link"
           href="https://reactjs.org"
